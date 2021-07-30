4d34f3d709ba0dfe11121b90203bc7488cdc4f19
quqiuzhu <quqiuzhu@qq.com>
2021-07-30 08:02:15 +0800

    bdelete without change window layout

---
+++ lua/ide/debug.lua
@@ -0,0 +1,5 @@
+-- https://github.com/mfussenegger/nvim-dap
+-- https://github.com/rcarriga/nvim-dap-ui
+-- https://github.com/Pocco81/DAPInstall.nvim
+-- https://github.com/sakhnik/nvim-gdb
+-- https://github.com/puremourning/vimspector
--- lua/ui/bufferline.lua
+++ lua/ui/bufferline.lua
@@ -11,8 +11,8 @@ local function setup()
             numbers = 'ordinal', -- "none" | "ordinal" | "buffer_id" | "both",
             number_style = 'superscript', -- "superscript" | "" | {"none", "subscript"}, -- buffer_id at index 1, ordinal at index 2
             mappings = true, -- true | false,
-            close_command = 'bdelete! %d', -- can be a string | function, see "Mouse actions"
-            right_mouse_command = 'bdelete! %d', -- can be a string | function, see "Mouse actions"
+            close_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
+            right_mouse_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
             left_mouse_command = 'buffer %d', -- can be a string | function, see "Mouse actions"
             middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
             -- NOTE: this plugin is designed with this icon in mind,
@@ -90,7 +90,13 @@ function bufferline:new()
 end
 
 function bufferline:plugins()
-    return {{'akinsho/nvim-bufferline.lua', config = setup, requires = 'kyazdani42/nvim-web-devicons'}}
+    return {
+        {
+            'akinsho/nvim-bufferline.lua',
+            config = setup,
+            requires = {'kyazdani42/nvim-web-devicons', 'famiu/bufdelete.nvim'}
+        }
+    }
 end
 
 function bufferline:config() end
