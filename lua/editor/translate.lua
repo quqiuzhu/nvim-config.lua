214ca0c68580ed4fecec44481f430c34d4526891
quqiuzhu <quqiuzhu@qq.com>
2021-08-17 18:21:27 +0800

    Fix: translate map use map api

--- lua/editor/translate.lua
+++ lua/editor/translate.lua
@@ -2,9 +2,9 @@
 local function setup()
     local g = {
         translator_target_lang = 'zh',
-        translator_default_engines = {'haici', 'youdao', 'bing', 'google'},
+        translator_default_engines = {'haici', 'bing'}, -- 'youdao','google'
         translator_proxy_url = '',
-        translator_history_enable = false,
+        translator_history_enable = true,
         translator_window_type = 'popup', -- ['popup', 'preview']
         translator_window_max_width = 0.6,
         translator_window_max_height = 0.6,
@@ -12,19 +12,13 @@ local function setup()
     }
     local config = require('common.config')
     config:set_vars(g)
-    vim.cmd [[
-        " Echo translation in the cmdline
-        nmap <silent> <Leader>t <Plug>Translate
-        vmap <silent> <Leader>t <Plug>TranslateV
-        " Display translation in a window
-        nmap <silent> <Leader>w <Plug>TranslateW
-        vmap <silent> <Leader>w <Plug>TranslateWV
-        " Replace the text with translation
-        " nmap <silent> <Leader>r <Plug>TranslateR
-        " vmap <silent> <Leader>r <Plug>TranslateRV
-        " Translate the text in clipboard
-        " nmap <silent> <Leader>x <Plug>TranslateX
-    ]]
+    -- Keybindings
+    local map = vim.api.nvim_set_keymap
+    local opts = {silent = true}
+    map('n', '<Leader>t', '<Plug>Translate', opts)
+    map('n', '<Leader>w', '<Plug>TranslateW', opts)
+    map('v', '<Leader>t', '<Plug>TranslateV', opts)
+    map('v', '<Leader>w', '<Plug>TranslateWV', opts)
 end
 
 local translate = {}
