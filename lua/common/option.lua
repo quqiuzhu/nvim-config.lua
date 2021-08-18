3eb9ac11a0fd2c0e7c2cec4cf63933212a6929a7
quqiuzhu <quqiuzhu@qq.com>
2021-08-18 15:07:30 +0800

    Fix: option timeoutlen is too short

--- lua/common/option.lua
+++ lua/common/option.lua
@@ -4,7 +4,7 @@ vim.o.shortmess = vim.o.shortmess .. 'c'
 vim.o.hidden = true
 vim.o.whichwrap = 'b,s,<,>,[,],h,l'
 vim.o.pumheight = 10
-vim.o.fileencoding = "utf-8"
+vim.o.fileencoding = 'utf-8'
 vim.o.splitbelow = true
 vim.o.termguicolors = true
 vim.o.splitright = true
@@ -14,15 +14,15 @@ vim.o.showmode = false
 vim.o.backup = false
 vim.o.writebackup = false
 vim.o.updatetime = 300
-vim.o.timeoutlen = 100
-vim.o.clipboard = "unnamedplus"
+vim.o.timeoutlen = 500 -- default 1000
+vim.o.clipboard = 'unnamedplus'
 vim.o.hlsearch = false
 vim.o.ignorecase = true
 vim.o.scrolloff = 3
 vim.o.sidescrolloff = 5
 
 -- Enable mouse to interact with editor.
-vim.o.mouse = "a"
+vim.o.mouse = 'a'
 
 -- Turn on line numbers
 vim.wo.wrap = false
@@ -30,10 +30,10 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.cursorline = true
 vim.wo.cursorcolumn = false
-vim.wo.signcolumn = "yes"
+vim.wo.signcolumn = 'yes'
 
 -- Enable command-line completion
-vim.o.wildmode = 'list:longest'
+-- vim.o.wildmode = 'list:longest'
 vim.o.cmdheight = 1
 
 -- When opening splits put them in correct positions
@@ -57,17 +57,17 @@ vim.o.expandtab = true
 vim.bo.expandtab = true
 
 -- Disable various builtin plugins in Vim that bog down speed
-vim.g.loaded_matchparen        = 1
-vim.g.loaded_matchit           = 1
-vim.g.loaded_logiPat           = 1
-vim.g.loaded_rrhelper          = 1
-vim.g.loaded_tarPlugin         = 1
-vim.g.loaded_gzip              = 1
-vim.g.loaded_zipPlugin         = 1
-vim.g.loaded_2html_plugin      = 1
-vim.g.loaded_shada_plugin      = 1
-vim.g.loaded_spellfile_plugin  = 1
-vim.g.loaded_netrw             = 1
-vim.g.loaded_netrwPlugin       = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_shada_plugin = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_tutor_mode_plugin = 1
-vim.g.loaded_remote_plugins    = 1
+vim.g.loaded_remote_plugins = 1
