7be3b9efa801503f01f7533877071ea5b7d55331
quqiuzhu <quqiuzhu@qq.com>
2022-05-28 07:12:59 +0800

    Feature: root pattern disable outmost

--- lua/ui/popup.lua
+++ lua/ui/popup.lua
@@ -76,19 +76,7 @@ function popup:plugins()
         },
         {
             'ygm2/rooter.nvim',
-            setup = function()
-                vim.g.rooter_pattern = {
-                    '.git',
-                    'Makefile',
-                    '_darcs',
-                    '.hg',
-                    '.bzr',
-                    '.svn',
-                    'node_modules',
-                    'CMakeLists.txt'
-                }
-                vim.g.outermost_root = true
-            end
+            setup = function() vim.g.rooter_pattern = {'.git', 'Makefile', '_darcs', '.hg', '.bzr', '.svn'} end
         }
     }
 end
