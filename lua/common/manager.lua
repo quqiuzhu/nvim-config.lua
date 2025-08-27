63d4a9b1fc4f445f0f3811c568bc9056ee9313e3
quqiuzhu <quqiuzhu@qq.com>
2025-08-27 10:41:05 +0800

    setup mirror for lazy.nvim and mason

--- lua/common/manager.lua
+++ lua/common/manager.lua
@@ -35,7 +35,9 @@ end
 function manager:_get_modules()
     for _, module_name in pairs(self.module_names) do
         local m = require(module_name)
-        if type(m) == 'table' then self.modules[module_name] = m:new() end
+        if type(m) == 'table' then
+            self.modules[module_name] = m:new()
+        end
     end
 end
 
@@ -55,15 +57,41 @@ end
 
 function manager:load_plugins()
     require('lazy').setup({
-        spec = self.plugins
+        spec = self.plugins,
         -- Configure any other settings here. See the documentation for more details.
         -- colorscheme that will be used when installing plugins.
         -- install = { colorscheme = { "habamax" } },
         -- automatically check for plugin updates
         -- checker = { enabled = true },
+        git = {
+            -- defaults for the `Lazy log` command
+            -- log = { "--since=3 days ago" }, -- show commits from the last 3 days
+            log = {'-8'}, -- show the last 8 commits
+            timeout = 120, -- kill processes that take more than 2 minutes
+            url_format = 'https://githubfast.com/%s.git',
+            -- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
+            -- then set the below to false. This should work, but is NOT supported and will
+            -- increase downloads a lot.
+            filter = true,
+            -- rate of network related git operations (clone, fetch, checkout)
+            throttle = {
+                enabled = false, -- not enabled by default
+                -- max 2 ops every 5 seconds
+                rate = 2,
+                duration = 5 * 1000 -- in ms
+            },
+            -- Time in seconds to wait before running fetch again for a plugin.
+            -- Repeated update/check operations will not run again until this
+            -- cooldown period has passed.
+            cooldown = 0
+        }
     })
 end
 
-function manager:set_configs() for _, m in pairs(self.modules) do m:config() end end
+function manager:set_configs()
+    for _, m in pairs(self.modules) do
+        m:config()
+    end
+end
 
 return manager
